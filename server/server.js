c7d350802089da58fdc21faa95f205f3f311a88a
Reutskiy <33157224+Reutskiy@users.noreply.github.com>
2019-01-22 02:42:10 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,8 +26,10 @@ mongoose.connect('mongodb://mio:deepbluesea777@ds163054.mlab.com:63054/easyquizy
 app.use(express.static(publicPath));
 
 //Starting server on port 3000
-server.listen(process.env.PORT || 5000) => {
-    console.log("Server started on port 3000");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
 //When a connection to server is made from client
