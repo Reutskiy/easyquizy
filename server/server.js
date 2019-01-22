b5a7a40c8f97591a401fbbefa2a4ba946385f4ec
Reutskiy <reutskiy@AndreyReutskiy.local>
2019-01-22 16:01:54 +0300

    1
    1

--- server/server.js
+++ server/server.js
@@ -16,15 +16,13 @@ var games = new LiveGames();
 var players = new Players();
 
 //Mongodb setup
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
-//var url = 'mongodb://mio:deepbluesea777@ds157834.mlab.com:57834/kahootdb';
-//mongoose.connect('mongodb://mio:deepbluesea777@ds163054.mlab.com:63054/easyquizy');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/polling');
+var url = "mongodb://localhost:27017/";
+mongoose.connect('mongodb://mio:deepbluesea777@ds163054.mlab.com:63054/easyquizy');
+
 
 app.use(express.static(publicPath));
-app.listen(process.env.PORT || 3000, () => console.log('Server has started'));
 
 //Starting server on port 3000
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
